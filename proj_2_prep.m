0e6878be48fb7cab64d9959ff2f874f694a3f133
axelholmgren <axel-eh@hotmail.com>
2023-12-05 14:08:04 +0100

    tandisläkispausis

--- proj_2_prep.m
+++ proj_2_prep.m
@@ -1,6 +1,6 @@
 %% load
 kth = load('kth.jpg');
-
+training = load('training.mat');
 %% encode pic prep 1 
 pic = imread('kth.jpg');
 
@@ -9,6 +9,20 @@ pic = imread('kth.jpg');
 %% Make some fucking noise! prep 2
 h = [1 0.7 0.7];
 dist_key = conv(key, h,'same');
+
+%% Equalizer
+L = 11;
+
+A = [];
+correct_bits = training(L+1:31);
+
+for row = 0:31-L;
+    for column = 0:L;
+        A(row+1,column+1) = dist_key(L+1-column+row);
+    end;
+end;
+g = correct_bits/A;
+
 
 
 %% decode pic prep 3
@@ -24,3 +38,11 @@ for n = 1:length(key)
     conv_result = conv(h,conv_vector)
     dsist_key= [dsist_key, conv_result]
 end
+
+%% test
+for row = 0:31-L;
+    for column = 0:L;
+        A(row+1,column+1) = dist_key(L+1-column+row);
+    end;
+end;
+g = correct_bits/A;
