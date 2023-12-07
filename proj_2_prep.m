488a712a704d391deecee486434682bebe977bb0
axelholmgren <axel-eh@hotmail.com>
2023-12-07 17:13:55 +0100

    Equalizer och detekor och skilda element klart

--- proj_2_prep.m
+++ proj_2_prep.m
@@ -1,48 +1,50 @@
 %% load
 kth = load('kth.jpg');
-training = load('training.mat');
-%% encode pic prep 1 
-pic = imread('kth.jpg');
+hamilton = load('hamilton.jpg');
+load('spydata.mat')
+load('training.mat');
+%% encode pic prep 1
+%pic = imread('kth.jpg');
+raw_pic = imread('hamilton.jpg');
+pic = imresize(raw_pic,[1024 1024]);
 
-[key,cPic] = encoder(pic);
+%[key,cPic] = encoder(pic);
 
 %% Make some fucking noise! prep 2
 h = [1 0.7 0.7];
-dist_key = conv(key, h,'same');
+dist_key = conv(received, h,'same');
 
 %% Equalizer
-L = 11;
-
+% Equalizer 
+L = 1;
+for i = 1:31
 A = [];
-correct_bits = training(L+1:31);
+correct_bits = training(L+1:32);
 
 for row = 0:31-L;
     for column = 0:L;
-        A(row+1,column+1) = dist_key(L+1-column+row);
+        A(row+1,column+1) = received(L+1-column+row);
     end;
 end;
-g = correct_bits/A;
+
+g = A\correct_bits;
 
 
+reconstructed_key = filter(g,1,received);
+mapped_key = sign(reconstructed_key);
+recon_pic = decoder(mapped_key, cPic);
 
-%% decode pic prep 3
-dPic = decoder(sign(dist_key), cPic);
-axis square;
-image(dPic);
+% Differentiated elements
+e = mapped_key(1:32) - training;
+E = nnz(e);
 
-%% HHHH
-dsist_key=[];
-for n = 1:length(key)
-    
-    conv_vector = [key()]
-    conv_result = conv(h,conv_vector)
-    dsist_key= [dsist_key, conv_result]
+subplot(5,7,i);
+image(recon_pic);
+title(['L= ',num2str(L),'E= ' ,num2str(E)]);
+L = L+1;
 end
 
-%% test
-for row = 0:31-L;
-    for column = 0:L;
-        A(row+1,column+1) = dist_key(L+1-column+row);
-    end;
-end;
-g = correct_bits/A;
+%% decode pic prep 3
+%dPic = decoder(dist_key, cPic);
+%axis square;
+%image(dPic);
